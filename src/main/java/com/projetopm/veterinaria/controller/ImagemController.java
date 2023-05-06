5b644c7a413cc44935afc2655accd4cf6f752fa8
KawanXLima <kawan.ximendes@gmail.com>
2023-05-06 17:06:22 -0300

    upload e exibicao de imagem

---
+++ src/main/java/com/projetopm/veterinaria/controller/ImagemController.java
@@ -0,0 +1,54 @@
+package com.projetopm.veterinaria.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.Files.copy;
+import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
+
+
+@RestController
+@RequestMapping("/api/imagem")
+public class ImagemController {
+
+    public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFiles(@RequestParam("file")MultipartFile file) throws IOException {
+
+            String filename = StringUtils.cleanPath(file.getOriginalFilename());
+            Path fileStorage = get(DIRECTORY, filename).toAbsolutePath().normalize();
+            copy(file.getInputStream(), fileStorage, REPLACE_EXISTING);
+            return ResponseEntity.ok().body(filename);
+
+    }
+
+    @GetMapping("exibir/{filename}")
+    public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+        Path filePath = get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+        if(!Files.exists(filePath)) {
+            throw new FileNotFoundException(filename + " was not found on the server");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add("File-Name", filename);
+        httpHeaders.add(CONTENT_DISPOSITION, "attachment;File-Name=" + resource.getFilename());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                .headers(httpHeaders).body(resource);
+    }
+
+
+}
